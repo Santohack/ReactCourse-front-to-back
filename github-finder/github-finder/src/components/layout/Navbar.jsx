ec575cd5416a14af3b9db80ae45dab5d07391a4f
santosh Maurya <santosh.maurya@wakencode.com>
2023-04-05 20:50:30 +0530

    Day -6 Completed (03/04/2023 - github-finder

---
+++ github-finder/github-finder/src/components/layout/Navbar.jsx
@@ -0,0 +1,13 @@
+import {FaGithub} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className='navbar mb-12'>
+      
+    </nav>
+  );
+}
+
+export default Navbar;
